6df27c327aa5d20d8dba4ce853d35f769c0b8e2e
Wilson Júnior <wilsonpjunior@gmail.com>
2013-11-01 15:09:16 -0200

    scripts do uwsgi

--- supportcenter/wsgi.py
+++ supportcenter/wsgi.py
@@ -16,11 +16,14 @@ framework.
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supportcenter.settings")
+from __init__ import setup_paths, connect_db
+setup_paths()
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
+connect_db()
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
